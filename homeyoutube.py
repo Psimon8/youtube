5f7519deab2c02d104e34c1f18ac5153222f8e41
Psimon8 <121551167+Psimon8@users.noreply.github.com>
2024-12-05 14:29:06 +0100

    Create homeyoutube.py

---
+++ homeyoutube.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+# Configuration de la page Streamlit
+st.set_page_config(
+    layout="wide",
+    page_title="SEO Youtube Box",
+    page_icon="📌"
+)
+
+# Streamlit UI
+st.title("SEO Tools Box")
+with st.sidebar:
+        youtube_api_key = st.text_input("Enter your YouTube API key:")
+        openai_api_key = st.text_input("Enter your OpenAI API key:")
+        
+        # Ajouter l'icône Twitter et le texte en bas à gauche
+st.sidebar.markdown(
+    """
+    <div style="position: fixed; bottom: 10px; left: 20px;">
+        <a href="https://github.com/Psimon8" target="_blank" style="text-decoration: none;">
+            <img src="https://github.githubassets.com/assets/pinned-octocat-093da3e6fa40.svg" 
+                 alt="GitHub Simon le Coz" style="width:20px; vertical-align: middle; margin-right: 5px;">
+            <span style="color: white; font-size: 14px;"></span>
+        </a>    
+        <a href="https://www.linkedin.com/in/simon-le-coz/" target="_blank" style="text-decoration: none;">
+            <img src="https://static.licdn.com/aero-v1/sc/h/8s162nmbcnfkg7a0k8nq9wwqo" 
+                 alt="LinkedIn Simon Le Coz" style="width:20px; vertical-align: middle; margin-right: 5px;">
+            <span style="color: white; font-size: 14px;"></span>
+        </a>
+        <a href="https://twitter.com/lekoz_simon" target="_blank" style="text-decoration: none;">
+            <img src="https://abs.twimg.com/favicons/twitter.3.ico" 
+                 alt="Twitter Simon Le Coz" style="width:20px; vertical-align: middle; margin-right: 5px;">
+            <span style="color: white; font-size: 14px;">@lekoz_simon</span>
+        </a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
